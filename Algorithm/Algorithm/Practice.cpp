d8d304fbbca69c8b97b8fd0fc5d33ab5c7b3f27b
박민현 <algusl92@naver.com>
2017-09-20 22:37:41 +0900

    숫자 개수 파악 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -4,35 +4,36 @@
 #include <math.h>
 #pragma warning(disable:4996)
 
-int main() {
+/*Timer 변수 선언*/
+clock_t before;
+double processTime;
 
-	/*Timer 변수 선언*/
-	clock_t before;
-	double processTime;
-	before = clock();
-
-	int cnt = 0;
-	char str[1000001];
-	fgets(str,sizeof(str),stdin);
-	str[strlen(str) - 1] = '\0';
-	
-	for (int i = 0; i < str[i]!='\0'; i++) {
-
-		if (str[i] != 32)
-		{
-			for (int j = i; str[j] != 32; j++)
-			{
-				if (str[j + 1] == 32){
-					cnt++;
-					i += j - i + 1;
-				}
-			}
-		}
-	}
-	printf("%d", cnt);
-
+void checkProcessTime() {
 	/*Timer 출력*/
 	processTime = (double)(clock() - before) / CLOCKS_PER_SEC;
 	printf("\n걸린시간은 %f입니다.\n", processTime);
+}
+
+int main() {
+	before = clock();
+
+	int A, B, C;
+	int result;
+	int checkNum[10] = { 0 };
+
+	scanf("%d %d %d", &A, &B, &C);
+	result = A*B*C;
+
+	while (result > 0) {
+		checkNum[result % 10] ++;
+		result /= 10;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d\n", checkNum[i]);
+	}
+
+	checkProcessTime();
 	return 0;
 }

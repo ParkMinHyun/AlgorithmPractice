e773c4ca678d555a6bfb0bd72d303d8a9b0116ab
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-09-20 12:00:24 +0900

    전치행렬 변환 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,18 +1,54 @@
-#include <iostream>
+#include <stdio.h>
 #include <time.h>
-using namespace std;
 
+#pragma warning(disable:4996)
 int main() {
+
+	/*Timer 변수 선언*/
 	clock_t before;
 	double result;
 	before = clock();
 
-	cout << "Hellow world" << endl;
+	int arr[10][10];
+	int num;
+	int i, j;
+	scanf("%d", &num);
 
+	for (i = 0; i<num; i++)
+	{
+		for (j = 0; j<num; j++)
+		{
+			scanf("%d", &arr[i][j]);
+		}
+	}
+
+	int temp;
+	for (i = 0; i<num; i++)
+	{
+		for (j = i + 1; j<num; j++)
+		{
+			temp = arr[i][j];
+			arr[i][j] = arr[j][i];
+			arr[j][i] = temp;
+		}
+	}
+
+	for (i = 0; i<num; i++)
+	{
+		for (j = 0; j<num; j++)
+		{
+			if (j == num - 1)
+				printf("%d", arr[i][j]);
+			else
+				printf("%d ", arr[i][j]);
+		}
+		if (i != num - 1)
+			printf("\n");
+	}
+
+
+	/*Timer 출력*/
 	result = (double)(clock() - before) / CLOCKS_PER_SEC;
-
-	printf("걸린시간은 %5.2f 입니다.n", result);
+	printf("\n걸린시간은 %f입니다.\n",result);
 	return 0;
-
-
 }

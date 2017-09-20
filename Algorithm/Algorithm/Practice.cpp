0eedf02cc5048b4fb434814cb3dee48e9d1b665c
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-09-20 16:46:39 +0900

    셀프넘버 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -6,30 +6,37 @@ int main() {
 
 	/*Timer 변수 선언*/
 	clock_t before;
-	double result;
+	double processTime;
 	before = clock();
 
-	int number;
-	int count = 0;
-	int A, B, C, Add;
-	scanf("%d", &number);
-	Add = number;
+	int middleResult = 0;
+	int result, sum, cnt = 0;
 
-	while (Add != number || count == 0) {
-		A = Add / 10;
-		B = Add % 10;
-		C = (A + B) % 10;
-		A = B; B = C;
-		Add = A * 10 + B;
-		count++;
+	for (int i = 1; i <= 10000; i++) {
+		cnt = 0;
+		for (int j = 1; j <= i; j++)
+		{
+			sum = j;
+			middleResult = j;
+
+			while (middleResult>0) {
+				sum += middleResult % 10;
+				middleResult = middleResult / 10;
+			}
+			if (sum == i){
+				cnt++;
+				break;
+			}
+		}
+		if(cnt == 0)
+			printf("%d\n", i);
 	}
-
-	printf("%d\n", count);
+	
 
 
 
 	/*Timer 출력*/
-	result = (double)(clock() - before) / CLOCKS_PER_SEC;
-	printf("\n걸린시간은 %f입니다.\n",result);
+	processTime = (double)(clock() - before) / CLOCKS_PER_SEC;
+	printf("\n걸린시간은 %f입니다.\n", processTime);
 	return 0;
 }

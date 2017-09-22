0ba7da55a0123e5d58dd4567436e623f53299f3f
박민현 <algusl92@naver.com>
2017-09-22 17:35:10 +0900

    Anagram 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -3,176 +3,43 @@
 #include <stdlib.h>
 #pragma warning(disable:4996);
 
-void reverseString(char* s)
-{
-	size_t size = strlen(s);
-	char temp;
-
-	for (size_t i = 0; i < size / 2; i++)
-	{
-		temp = s[i];
-		s[i] = s[(size - 1) - i];
-		s[(size - 1) - i] = temp;
-	}
-}
-
 int main()
 {
-	int year = 2000;
-	int month = 1;
-	int day = 1;
-	int hour = 0;
-	int min = 0;
-	int sec = 0;
-	int cnt = 0;
-	int input;
+	int num,sum, alphabet[26] = { 0 }, alphabet2[26] = { 0 };
+	char str1[101], str2[101];
 
-	char yearStr[5];
-	char monthStr[20];
-	char dayStr[20];
-	char hourStr[20];
-	char minStr[20];
-	char secStr[20];
-	char output[20];
-	char outputReverse[20];
-	char t1[2] = "0";
-	char tmp[20];
-
-	int num;
-
-	scanf("%d", &num);
+	scanf("%d ", &num);
 	for (int i = 0; i < num; i++) {
-
-		scanf("%d", &input);
-
-		while (1)
-		{
-			sec++;
-
-
-			if (sec == 60)
-			{
-				sec = 0;
-				min++;
-			}
-
-			if (min == 60)
-			{
-				min = 0;
-				hour++;
-			}
-
-			if (hour == 24)
-			{
-				hour = 0;
-				day++;
-			}
-
-			if (day == 30 && month == 2 && year % 4 == 0 && year % 400 == 0)
-			{
-				day = 1;
-				month++;
-			}
-			else if (day == 31 && (month == 4 || month == 6 || month == 9 || month == 11))
-			{
-				day = 1;
-				month++;
-			}
-			else if (day == 32 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12))
-			{
-				day = 1;
-				month++;
-			}
-			else if (day == 29 && month == 2 && year % 100 == 0 && year % 400 != 0)
-			{
-				day = 1;
-				month++;
-			}
-
-
-			if (month == 13)
-			{
-				year++;
-				month = 1;
-			}
-
-			snprintf(yearStr, sizeof(yearStr), "%d", year);
-			if (sec < 10)
-			{
-				if (yearStr[0] != secStr[0] || yearStr[1] != '0')
-					continue;
-
-				tmp[0] = '\0';
-				strcpy(tmp, t1);
-				strcat(tmp, secStr);
-				strcpy(secStr, tmp);
-			}
-
-			if (yearStr[0] != secStr[1] || yearStr[1] != secStr[0])
-				continue;
-
-			snprintf(minStr, sizeof(minStr), "%d", min);
-			if (min < 10)
-			{
-				if (yearStr[2] != minStr[0] || yearStr[3] != '0')
-					continue;
-				tmp[0] = '\0';
-				strcpy(tmp, t1);
-				strcat(tmp, minStr);
-				strcpy(minStr, tmp);
-			}
-			if (yearStr[2] != minStr[1] || yearStr[3] != minStr[0])
-				continue;
-
-			//2000-11-11 11:00:02
-
-			snprintf(monthStr, sizeof(monthStr), "%d", month);
-			snprintf(hourStr, sizeof(hourStr), "%d", hour);
-			if (month < 10)
-			{
-				tmp[0] = '\0';
-				strcpy(tmp, t1);
-				strcat(tmp, monthStr);
-				strcpy(monthStr, tmp);
-			}
-
-			if (hour < 10)
-			{
-				tmp[0] = '\0';
-				strcpy(tmp, t1);
-				strcat(tmp, hourStr);
-				strcpy(hourStr, tmp);
-			}
-			if (monthStr[0] != hourStr[1] || monthStr[1] != hourStr[0])
-				continue;
-
-
-
-			snprintf(dayStr, sizeof(dayStr), "%d", day);
-
-
-			if (day < 10)
-			{
-				tmp[0] = '\0';
-				strcpy(tmp, t1);
-				strcat(tmp, dayStr);
-				strcpy(dayStr, tmp);
-			}
-
-			sprintf(output, "%s%s%s%s%s%s", yearStr, monthStr, dayStr, hourStr, minStr, secStr);
-			strcpy(outputReverse, output);
-			reverseString(outputReverse);
-
-			if (strcmp(outputReverse, output) == 0)
-			{
-				cnt++;
-
-				if (cnt == input)
-				{
-					printf("%s-%s-%s %s:%s:%s\n", yearStr, monthStr, dayStr, hourStr, minStr, secStr);
-					break;
-				}
-			}
+		sum = 0;
+		for (int j = 0; j < 26; j++){
+			alphabet[j] = 0;
+			alphabet2[j] = 0;
 		}
+
+		scanf("%s %s", str1, str2);
+		if (!strcmp(str1, str2)){
+			printf("No.\n");
+			continue;
+		}
+
+		for (int j = 0; j < strlen(str1); j++){
+			if('a' <= str1[j] && str1[j] <= 'z')
+				alphabet[str1[j] - 97] ++;
+			else
+				alphabet[str1[j] - 65] ++;
+		}
+		
+		for (int j = 0; j < strlen(str2); j++)
+			if ('a' <= str2[j] && str2[j] <= 'z')
+				alphabet2[str2[j] - 97] ++;
+			else
+				alphabet2[str2[j] - 65] ++;
+		
+		for (int j = 0; j < 26; j++)
+			if ( (alphabet[j] == alphabet2[j]) && alphabet[j] != 0)
+				sum += alphabet[j];
+
+		
+		printf("%s\n",(sum!=strlen(str1))?"No.":"Yes");
 	}
 }

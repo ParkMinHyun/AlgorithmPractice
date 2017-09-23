7baa3c6b5485b5a32b532fb4cf63e4c1d3c77056
박민현 <algusl92@naver.com>
2017-09-23 11:51:50 +0900

    암호화, 복호화 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -3,43 +3,77 @@
 #include <stdlib.h>
 #pragma warning(disable:4996);
 
-char str1[101], str2[101];
-
-void sort(char *str) {
-	char temp;
-	for (int i = 0; i < strlen(str); i++) {
-		for (int j = i + 1; j < strlen(str); j++) {
-			if (str[i] < str[j])
-			{
-				temp = str[i];
-				str[i] = str[j];
-				str[j] = temp;
-			}
-		}
-	}
+void leftRotatebyOne(char arr[], int n)
+{
+	int i, temp;
+	temp = arr[0];
+	for (i = 0; i < n - 1; i++)
+		arr[i] = arr[i + 1];
+	arr[i] = temp;
 }
+void leftRotate(char arr[], int d, int n)
+{
+	int i;
+	for (i = 0; i < d; i++)
+		leftRotatebyOne(arr, n);
+}
+
+void rightRotatebyOne(char arr[], int n)
+{
+	int i, temp;
+	temp = arr[strlen(arr)-1];
+	for (i = n-1; i > 0; i--)
+		arr[i] = arr[i-1];
+	arr[0] = temp;
+}
+void rightRotate(char arr[], int d, int n)
+{
+	int i;
+	for (i = 0; i < d; i++)
+		rightRotatebyOne(arr, n);
+}
+
+
 int main()
 {
-	int num;
+	char str1[8], str2[32], str3[32],result[31];
+	int rotate;
 
-	scanf("%d ", &num);
-	for (int i = 0; i < num; i++) {
+	scanf("%s %s %d %s", str1, str2, &rotate, str3);
 
-		scanf("%s %s", str1, str2);
-		if (!strcmp(str1, str2)) {
-			printf("No.");
-			printf("\n");
-			continue;
+	if (!strcmp(str1, "encrypt")) {
+		for (int i = 0; i <= strlen(str2); i++) {
+			if(str2[i] + (str3[i] - 97) > 122)
+				result[i] = str2[i] + (str3[i] - 97) - 26;
+			else
+				result[i] = str2[i] + str3[i] - 97;
 		}
 
-		sort(str1);
-		sort(str2);
+		result[strlen(str2)] = '\0';
 
-		if (!strcmp(str1, str2))
-			printf("Yes");
+		if (rotate < 0)
+			rightRotate(result, -rotate, strlen(result));
 		else
-			printf("No.");
+			leftRotate(result, rotate, strlen(result));
 
-		printf("\n");
 	}
+	else if(!strcmp(str1, "decrypt")) {
+
+		if (rotate < 0)
+			leftRotate(str3, -rotate, strlen(str3));
+		else
+			rightRotate(str3, rotate, strlen(str3));
+
+		for (int i = 0; i <= strlen(str2); i++) {
+			if (str3[i] - str2[i] > 0)
+				result[i] = 'a' + str3[i] - str2[i];
+			else
+				result[i] = str3[i] - str2[i] + 123;
+		}
+	}
+	//encrypt secretword -3 helloworld
+	//decrypt secretword 3 cspkfcgzin
+	for(int i =0; i<strlen(str3); i++)
+		printf("%c", result[i]);
+	printf("\n");
 }

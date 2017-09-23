d124b2bf5e0ca5a8cb85e0b16f166ce7d07501b6
박민현 <algusl92@naver.com>
2017-09-23 09:52:04 +0900

    Anagram 알고리즘 ( 버블 Sort 이용 )

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -3,43 +3,43 @@
 #include <stdlib.h>
 #pragma warning(disable:4996);
 
+char str1[101], str2[101];
+
+void sort(char *str) {
+	char temp;
+	for (int i = 0; i < strlen(str); i++) {
+		for (int j = i + 1; j < strlen(str); j++) {
+			if (str[i] < str[j])
+			{
+				temp = str[i];
+				str[i] = str[j];
+				str[j] = temp;
+			}
+		}
+	}
+}
 int main()
 {
-	int num,sum, alphabet[26] = { 0 }, alphabet2[26] = { 0 };
-	char str1[101], str2[101];
+	int num;
 
 	scanf("%d ", &num);
 	for (int i = 0; i < num; i++) {
-		sum = 0;
-		for (int j = 0; j < 26; j++){
-			alphabet[j] = 0;
-			alphabet2[j] = 0;
-		}
 
 		scanf("%s %s", str1, str2);
-		if (!strcmp(str1, str2)){
-			printf("No.\n");
+		if (!strcmp(str1, str2)) {
+			printf("No.");
+			printf("\n");
 			continue;
 		}
 
-		for (int j = 0; j < strlen(str1); j++){
-			if('a' <= str1[j] && str1[j] <= 'z')
-				alphabet[str1[j] - 97] ++;
-			else
-				alphabet[str1[j] - 65] ++;
-		}
-		
-		for (int j = 0; j < strlen(str2); j++)
-			if ('a' <= str2[j] && str2[j] <= 'z')
-				alphabet2[str2[j] - 97] ++;
-			else
-				alphabet2[str2[j] - 65] ++;
-		
-		for (int j = 0; j < 26; j++)
-			if ( (alphabet[j] == alphabet2[j]) && alphabet[j] != 0)
-				sum += alphabet[j];
+		sort(str1);
+		sort(str2);
 
-		
-		printf("%s\n",(sum!=strlen(str1))?"No.":"Yes");
+		if (!strcmp(str1, str2))
+			printf("Yes");
+		else
+			printf("No.");
+
+		printf("\n");
 	}
 }

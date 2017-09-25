b10082d54c2a4e97250ffe8751532a1fa82c4d36
박민현 <algusl92@naver.com>
2017-09-25 22:27:14 +0900

    단어 공부 알고리즘 ( 최대 빈도수 알파벳 찾기 )

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,79 +1,40 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+
 #pragma warning(disable:4996);
 
-void leftRotatebyOne(char arr[], int n)
-{
-	int i, temp;
-	temp = arr[0];
-	for (i = 0; i < n - 1; i++)
-		arr[i] = arr[i + 1];
-	arr[i] = temp;
-}
-void leftRotate(char arr[], int d, int n)
-{
-	int i;
-	for (i = 0; i < d; i++)
-		leftRotatebyOne(arr, n);
-}
+int main() {
 
-void rightRotatebyOne(char arr[], int n)
-{
-	int i, temp;
-	temp = arr[strlen(arr)-1];
-	for (i = n-1; i > 0; i--)
-		arr[i] = arr[i-1];
-	arr[0] = temp;
-}
-void rightRotate(char arr[], int d, int n)
-{
-	int i;
-	for (i = 0; i < d; i++)
-		rightRotatebyOne(arr, n);
-}
+	int alphabet[26] = { 0 };
+	char str[1000000];
+	int len, flag = 0, max = 0;
+	char maxValue;
 
+	scanf("%s", str);
+	len = strlen(str);
 
-int main()
-{
-	char str1[8], str2[32], str3[32],result[31];
-	int rotate;
+	for (int i = 0; i < len; i++)
+	{
+		if ('a' <= str[i] && str[i] <= 'z') 
+			str[i] = str[i] - 32;
+		alphabet[str[i]-65] ++;
 
-	scanf("%s %s %d %s", str1, str2, &rotate, str3);
-
-	if (!strcmp(str1, "encrypt")) {
-		for (int i = 0; i <= strlen(str2); i++) {
-			if(str2[i] + (str3[i] - 97) > 122)
-				result[i] = str2[i] + (str3[i] - 97) - 26;
-			else
-				result[i] = str2[i] + str3[i] - 97;
-		}
-
-		result[strlen(str2)] = '\0';
-
-		if (rotate < 0)
-			rightRotate(result, -rotate, strlen(result));
-		else
-			leftRotate(result, rotate, strlen(result));
-
-	}
-	else if(!strcmp(str1, "decrypt")) {
-
-		if (rotate < 0)
-			leftRotate(str3, -rotate, strlen(str3));
-		else
-			rightRotate(str3, rotate, strlen(str3));
-
-		for (int i = 0; i <= strlen(str2); i++) {
-			if (str3[i] - str2[i] > 0)
-				result[i] = 'a' + str3[i] - str2[i];
-			else
-				result[i] = str3[i] - str2[i] + 123;
+		if (max < alphabet[str[i] - 65]){
+			max = alphabet[str[i] - 65];
+			maxValue = str[i];
 		}
 	}
-	//encrypt secretword -3 helloworld
-	//decrypt secretword 3 cspkfcgzin
-	for(int i =0; i<strlen(str3); i++)
-		printf("%c", result[i]);
-	printf("\n");
+
+	for (int i = 0; i < 26; i++)
+	{
+		if ( max == alphabet[i]){
+			flag ++;
+		}
+	}
+
+	if (flag > 1)
+		printf("?\n");
+	else
+		printf("%c\n", maxValue );
+
 }

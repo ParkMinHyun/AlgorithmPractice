1a5ba536846134cb29c393ae377d58750bae674e
박민현 <algusl92@naver.com>
2017-09-27 14:51:33 +0900

    중앙값 측정 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,34 +1,57 @@
-#include <iostream>
-#include <cstdio>
-#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-using namespace std;
+#pragma warning(disable:4996)
 
-string str;
-int main()
-{
-	cin >> str;
-	int pos = 0;
+int sum = 0;
+void sort(int *onDo, int start, int K, int index) {
+	int temp, i, j, l;
+	int *tempOnDo;
+	tempOnDo = (int *)malloc(sizeof(int)*(K+1));
+	for (l = 0; l < K; l++){
+		tempOnDo[l] = onDo[l+start];
+		//printf("%d ", tempOnDo[l]);
+	}
 
-	string croatia[8] = { "c=","c-","dz=","d-","lj","nj","s=","z=" };
-	string del;
-	string star = "*";
-
-	for (int i = 0; i < 8; i++)
-	{
-		del = croatia[i];
-
-		if (str.find(del) != string::npos)
-		{
-			while ((pos = str.find(del)) != string::npos)
-			{
-				str.erase(pos, del.length());
-				str.insert(pos, star);
+	for (i = 0; i < K - 1; i++) {
+		for (j = 0; j < K - 1 - i; j++) {
+			if (tempOnDo[j] > tempOnDo[j + 1]) {
+				temp = tempOnDo[j];
+				tempOnDo[j] = tempOnDo[j + 1];
+				tempOnDo[j + 1] = temp;
 			}
 		}
 	}
 
-	cout << str.size() << endl;
+	/*printf("-- 정렬 후 :");
+	for (i = 0; i < K; i++) {
+		printf("%d ", tempOnDo[i]);
+	}*/
 
+	sum += tempOnDo[index - 1];
+	//printf("중간값: %d , 중간합: %d\n ", tempOnDo[index-1], sum);
+}
+
+int main()
+{
+	int N, K, i = 0, index;
+	int *onDo;
+
+	scanf("%d %d", &N, &K);
+	index = (K + 1) / 2;
+	onDo = (int *)malloc(sizeof(int)*(N+1));
+	while (i < N) {
+		scanf("%d", &onDo[i]);
+
+		if (i < K - 1) {
+			i += 1;
+			continue;
+		}
+
+		sort(onDo, (i + 1) - K, K, index);
+		i += 1;
+	}
+	printf("%d", sum);
 	return 0;
 }

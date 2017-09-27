6acd5b04ef000fad0dfff4aeb3f5c14225d7cbe6
박민현 <algusl92@naver.com>
2017-09-27 17:32:16 +0900

    파일 입출력 기본 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,57 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-
-#pragma warning(disable:4996)
-
-int sum = 0;
-void sort(int *onDo, int start, int K, int index) {
-	int temp, i, j, l;
-	int *tempOnDo;
-	tempOnDo = (int *)malloc(sizeof(int)*(K+1));
-	for (l = 0; l < K; l++){
-		tempOnDo[l] = onDo[l+start];
-		//printf("%d ", tempOnDo[l]);
-	}
-
-	for (i = 0; i < K - 1; i++) {
-		for (j = 0; j < K - 1 - i; j++) {
-			if (tempOnDo[j] > tempOnDo[j + 1]) {
-				temp = tempOnDo[j];
-				tempOnDo[j] = tempOnDo[j + 1];
-				tempOnDo[j + 1] = temp;
-			}
-		}
-	}
-
-	/*printf("-- 정렬 후 :");
-	for (i = 0; i < K; i++) {
-		printf("%d ", tempOnDo[i]);
-	}*/
-
-	sum += tempOnDo[index - 1];
-	//printf("중간값: %d , 중간합: %d\n ", tempOnDo[index-1], sum);
-}
+#pragma warning(disable: 4996)
 
 int main()
 {
-	int N, K, i = 0, index;
-	int *onDo;
+	char from_a_txt[100];
+	FILE *fileIn, *fileOut;
+	fileIn = fopen("aa.txt", "r");
+	fileOut = fopen("bb.txt", "w");
 
-	scanf("%d %d", &N, &K);
-	index = (K + 1) / 2;
-	onDo = (int *)malloc(sizeof(int)*(N+1));
-	while (i < N) {
-		scanf("%d", &onDo[i]);
-
-		if (i < K - 1) {
-			i += 1;
-			continue;
-		}
-
-		sort(onDo, (i + 1) - K, K, index);
-		i += 1;
+	if (fileIn != nullptr) {
+		fscanf(fileIn, "%s", from_a_txt);
+		fprintf(fileOut, "%s", from_a_txt);
+		fclose(fileIn);
+		fclose(fileOut);
 	}
-	printf("%d", sum);
+
 	return 0;
 }

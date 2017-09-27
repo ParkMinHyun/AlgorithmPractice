4a5c45499134f8eaa66c29542b92fa54f6394e98
박민현 <algusl92@naver.com>
2017-09-27 20:41:20 +0900

    크로아티아 알파벳 알고리즘 수정 ( C )

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,35 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#pragma warning(disable: 4996)
 
-#pragma warning(disable:4996)
 void leftMovebyOne(char arr[], int start, int end);
-
 int main()
 {
-	char str[100];
+	char str[101];
 	char croatia[8][4] = { { "c=" },{ "c-" },{ "dz=" },{ "d-" },{ "lj" },{ "nj" },{ "s=" },{ "z=" } };
-	/*c=es=njak*/
 	
 	scanf("%s", str);
 	int size = strlen(str);
 	for (int i = 0; i < 8; i++)
 	{
-		char temp[100];
+		char *temp;
 		for (int j = 0; j < size - 1; j++) {
+			char str2[101];
+
 			if (str[j] == 'd' && str[j + 1] == 'z')
 			{
-				if (j == size - 2)
+				if (j != size - 2) {
 					if (str[j + 2] == '=') {
 						str[j] = '*';
-						leftMovebyOne(str, j, strlen(str));
-						leftMovebyOne(str, j, strlen(str));
+						leftMovebyOne(str, j + 1, size);
+						leftMovebyOne(str, j + 1, size);
 					}
+				}
 			}
 			else {
-				sprintf_s(temp, "%c%c", str[j], str[j + 1]);
-				if (!strcmp(croatia[i], temp)) {
+				sprintf(str2, "%c%c", str[j], str[j + 1]);
+				if (!strcmp(croatia[i], str2)) {
 					str[j] = '*';
-					leftMovebyOne(str,j+1,strlen(str));
+					leftMovebyOne(str, j + 1, size);
 				}
 			}
 		}
@@ -39,11 +40,9 @@ int main()
 
 	return 0;
 }
-
-
 void leftMovebyOne(char arr[], int start, int end)
 {
-	int i;
+	int i;;
 	for (i = start; i < end; i++)
 		arr[i] = arr[i + 1];
 }

b7d749a46ddb5f7331b91b2a940012874ef06e07
박민현 <algusl92@naver.com>
2017-09-27 20:26:01 +0900

    크로아티아 알파벳 알고리즘 ( C )

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,26 +1,49 @@
 #include <stdio.h>
 #include <string.h>
-#pragma warning(disable: 4996)
+
+#pragma warning(disable:4996)
+void leftMovebyOne(char arr[], int start, int end);
 
 int main()
 {
-	char ch;
-	char from_a_txt[100];
-	FILE *fileIn, *fileOut;
-
-	fileIn = fopen("aa.txt", "r");
-	fileOut = fopen("bb.txt", "w");
-
-	if (fileIn != nullptr) {
-		while (fscanf(fileIn, "%c", &ch) != EOF) {
-			fprintf(fileOut, "%c", ch);
-
+	char str[100];
+	char croatia[8][4] = { { "c=" },{ "c-" },{ "dz=" },{ "d-" },{ "lj" },{ "nj" },{ "s=" },{ "z=" } };
+	/*c=es=njak*/
+	
+	scanf("%s", str);
+	int size = strlen(str);
+	for (int i = 0; i < 8; i++)
+	{
+		char temp[100];
+		for (int j = 0; j < size - 1; j++) {
+			if (str[j] == 'd' && str[j + 1] == 'z')
+			{
+				if (j == size - 2)
+					if (str[j + 2] == '=') {
+						str[j] = '*';
+						leftMovebyOne(str, j, strlen(str));
+						leftMovebyOne(str, j, strlen(str));
+					}
+			}
+			else {
+				sprintf_s(temp, "%c%c", str[j], str[j + 1]);
+				if (!strcmp(croatia[i], temp)) {
+					str[j] = '*';
+					leftMovebyOne(str,j+1,strlen(str));
+				}
+			}
 		}
-		/*fgets(from_a_txt, 150, fileIn);
-		fputs(from_a_txt, fileOut);*/
-		//fprintf(fileOut, "%s", from_a_txt);
-		fclose(fileIn);
-		fclose(fileOut);
 	}
+
+	printf("%d\n", strlen(str));
+
 	return 0;
+}
+
+
+void leftMovebyOne(char arr[], int start, int end)
+{
+	int i;
+	for (i = start; i < end; i++)
+		arr[i] = arr[i + 1];
 }

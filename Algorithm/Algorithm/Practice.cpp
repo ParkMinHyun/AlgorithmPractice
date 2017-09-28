5f3693d885479f3f89dd2caafaa1a209a206510e
박민현 <algusl92@naver.com>
2017-09-28 15:04:23 +0900

    Hanoi Tower 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,48 +1,22 @@
-#include <stdio.h>
-#include <string.h>
+
+#include<stdio.h>
 #pragma warning(disable: 4996)
-
-void leftMovebyOne(char arr[], int start, int end);
-int main()
+void hanoi(int n, char from, char temp, char to)
 {
-	char str[101];
-	char croatia[8][4] = { { "c=" },{ "c-" },{ "dz=" },{ "d-" },{ "lj" },{ "nj" },{ "s=" },{ "z=" } };
-	
-	scanf("%s", str);
-	int size = strlen(str);
-	for (int i = 0; i < 8; i++)
+	if (n == 1)
+		printf("%d %c %c\n", 1,from, to);
+	else
 	{
-		char *temp;
-		for (int j = 0; j < size - 1; j++) {
-			char str2[101];
+		hanoi(n - 1, from, to, temp);
+		printf("%d %c %c\n", n, from, to);
+		hanoi(n - 1, temp, from, to);
 
-			if (str[j] == 'd' && str[j + 1] == 'z')
-			{
-				if (j != size - 2) {
-					if (str[j + 2] == '=') {
-						str[j] = '*';
-						leftMovebyOne(str, j + 1, size);
-						leftMovebyOne(str, j + 1, size);
-					}
-				}
-			}
-			else {
-				sprintf(str2, "%c%c", str[j], str[j + 1]);
-				if (!strcmp(croatia[i], str2)) {
-					str[j] = '*';
-					leftMovebyOne(str, j + 1, size);
-				}
-			}
-		}
 	}
-
-	printf("%d\n", strlen(str));
-
-	return 0;
 }
-void leftMovebyOne(char arr[], int start, int end)
-{
-	int i;;
-	for (i = start; i < end; i++)
-		arr[i] = arr[i + 1];
+int main() {
+
+	int num;
+	scanf("%d", &num);
+	hanoi(num, 'A', 'B', 'C');
+	return 0;
 }

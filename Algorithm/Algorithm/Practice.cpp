34eb08f808d007b7eec678cb3c68b410df9ac53e
박민현 <algusl92@naver.com>
2017-09-28 23:59:23 +0900

    선택 정렬

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,22 +1,39 @@
 
 #include<stdio.h>
-#pragma warning(disable: 4996)
-void hanoi(int n, char from, char temp, char to)
-{
-	if (n == 1)
-		printf("%d %c %c\n", 1,from, to);
-	else
-	{
-		hanoi(n - 1, from, to, temp);
-		printf("%d %c %c\n", n, from, to);
-		hanoi(n - 1, temp, from, to);
+#include <string.h>
+#include <stdlib.h>
 
+#pragma warning(disable: 4996)
+
+void swap(int *a, int *b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void selectionSort(int arr[]) {
+
+	int i;
+	int temp;
+	for (i = 0; i < 9; i++) {
+		temp = i;
+		for (int j = i + 1; j < 10; j++) {
+			if (arr[temp] > arr[j])
+				temp = j;
+		}
+		swap(&arr[temp], &arr[i]);
+		
 	}
 }
 int main() {
+	int arr[10] = { 11,1,6,6,55,13,24,2,31,19 };
 
-	int num;
-	scanf("%d", &num);
-	hanoi(num, 'A', 'B', 'C');
+	selectionSort(arr);
+
+	for (int i = 0; i < 10; i++)
+		printf("%d ",arr[i]);
+	printf("\n");
+
 	return 0;
 }

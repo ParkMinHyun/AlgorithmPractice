c3e24432fc2e742c603b51643a71ab7d049e1565
박민현 <algusl92@naver.com>
2017-09-29 00:33:07 +0900

    합병 정렬 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -11,26 +11,45 @@ void swap(int *a, int *b) {
 	*b = temp;
 }
 
-void insertionSort(int arr[]) {
+void merge(int arr[], int start, int mid, int end) {
+	int buf[11] = { 0 };
 
-	int i,j;
-	int temp;
-	for (i = 1; i < 10; i++) {
-		j = i - 1;
-		while (j >= 0 && arr[j] > arr[j + 1]) {
-			swap(&arr[j], &arr[j+1]);
-			j--;
-		}
+	int i = start, j = mid+1, k = start;
+	while (i <= mid && j <= end) {
+		if (arr[i] < arr[j])
+			buf[k++] = arr[i++];
+		else
+			buf[k++] = arr[j++];
+	}
+
+	while (i <= mid)
+		buf[k++] = arr[i++];
+
+	while (j <= end)
+		buf[k++] = arr[j++];
+
+	for (i = start; i <= end; i++)
+		arr[i] = buf[i];
+
+}
+void mergeSort(int arr[], int start, int end) {
+
+	int mid = (start + end) / 2;
+
+	if (start < end) {
+		mergeSort(arr, start, mid);
+		mergeSort(arr, mid + 1, end);
+		merge(arr, start, mid, end);
 	}
 }
 
 int main() {
 	int arr[10] = { 11,1,6,4,55,13,24,2,31,19 };
 
-	insertionSort(arr);
+	mergeSort(arr, 0, 9);
 
 	for (int i = 0; i < 10; i++)
-		printf("%d ",arr[i]);
+		printf("%d ", arr[i]);
 	printf("\n");
 
 	return 0;

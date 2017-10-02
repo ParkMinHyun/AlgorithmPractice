2b0f566871b0433b2bc54d59abb4e0ea81199dc5
박민현 <algusl92@naver.com>
2017-10-03 01:06:20 +0900

    단어 정렬 알고리즘 - 시간 초과

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -3,23 +3,32 @@
 #include <stdlib.h>
 #pragma warning(disable: 4996)
 
-void lenSort(char str[][51],int size) {
-	int len1, len2;
-	char *temp;
+void Sort(char str[][51], int size) {
+	int len1, len2, k;
+	char temp[51];
 
-	for(int i=0; i< size-1; i++){
-		for(int j=0; j<size-1-i; j++){
+	for (int i = 0; i< size - 1; i++) {
+		for (int j = 0; j<size - 1 - i; j++) {
 			len1 = strlen(str[j]);
-			len2 = strlen(str[j+1]);
-			if (len1 > len2 || (strcmp(str[j],str[j+1])<1 && len1==len2))
+			len2 = strlen(str[j + 1]);
+			if (len1 > len2 || (strcmp(str[j], str[j + 1])<1 && len1 == len2))
 			{
-				temp = str[j];
-				strcpy(str[j], str[j + 1]);
-				strcpy(str[j+1], temp);
+				for (k = 0; k < len1; k++)
+					temp[k] = str[j][k];
+				temp[len1] = '\0';
+
+				for (k = 0; k < len2; k++)
+					str[j][k] = str[j + 1][k];
+				str[j][len2] = '\0';
+
+				for (k = 0; k < len1; k++)
+					str[j + 1][k] = temp[k];
+				str[j + 1][len1] = '\0';
 			}
 		}
 	}
 }
+
 int main() {
 	int num;
 	char str[20000][51];
@@ -28,8 +37,11 @@ int main() {
 	for (int i = 0; i < num; i++)
 		scanf("%s", str[i]);
 
-	lenSort(str,num);
+	Sort(str, num);
 
-	for (int i = 0; i < num; i++)
-		scanf("%s", str[i]);
+	for (int j = 0; j<num; j++) {
+		if (strcmp(str[j], str[j + 1])) {
+			printf("%s\n", str[j]);
+		}
+	}
 }

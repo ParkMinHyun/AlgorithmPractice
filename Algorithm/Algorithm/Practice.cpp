ed9ec65d3a7af49bbb6866ad94818da7e425c24e
박민현 <algusl92@naver.com>
2017-10-03 02:51:23 +0900

    단어 정렬 알고리즘 - 구조체 이용

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -3,45 +3,44 @@
 #include <stdlib.h>
 #pragma warning(disable: 4996)
 
-void Sort(char str[][51], int size) {
-	int len1, len2, k;
-	char temp[51];
+typedef struct WORDBOOK
+{
+	int len;
+	char word[51];
+}wordBook;
 
-	for (int i = 0; i< size - 1; i++) {
-		for (int j = 0; j<size - 1 - i; j++) {
-			len1 = strlen(str[j]);
-			len2 = strlen(str[j + 1]);
-			if (len1 > len2 || (strcmp(str[j], str[j + 1])<1 && len1 == len2))
+wordBook wb[20001];
+
+void Sort(int size) {
+	int k;
+	wordBook w;
+
+	for (int i = 0; i < size - 1; i++)
+		for (int j = 0; j < size - 1 - i; j++)
+			if (wb[j].len > wb[j + 1].len ||
+				(strcmp(wb[j].word, wb[j + 1].word) == 1 && wb[j].len == wb[j + 1].len))
 			{
-				for (k = 0; k < len1; k++)
-					temp[k] = str[j][k];
-				temp[len1] = '\0';
-
-				for (k = 0; k < len2; k++)
-					str[j][k] = str[j + 1][k];
-				str[j][len2] = '\0';
-
-				for (k = 0; k < len1; k++)
-					str[j + 1][k] = temp[k];
-				str[j + 1][len1] = '\0';
+				w = wb[j];
+				wb[j] = wb[j + 1];
+				wb[j + 1] = w;
 			}
-		}
-	}
 }
 
 int main() {
 	int num;
-	char str[20000][51];
 
 	scanf("%d", &num);
-	for (int i = 0; i < num; i++)
-		scanf("%s", str[i]);
-
-	Sort(str, num);
-
-	for (int j = 0; j<num; j++) {
-		if (strcmp(str[j], str[j + 1])) {
-			printf("%s\n", str[j]);
-		}
+	for (int i = 0; i < num; i++) {
+		scanf("%s", wb[i].word);
+		wb[i].len = strlen(wb[i].word);
 	}
+
+	Sort(num);
+
+	for (int j = 0; j < num; j++) {
+		if (strcmp(wb[j].word, wb[j + 1].word))
+			printf("%s\n", wb[j].word);
+	}
+
+	return 0;
 }

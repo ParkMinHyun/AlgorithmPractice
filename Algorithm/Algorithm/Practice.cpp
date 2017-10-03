f0ddec52de79200d343e5a63a69e88d26df5fa2d
박민현 <algusl92@naver.com>
2017-10-03 17:37:21 +0900

    Stack 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,19 +1,67 @@
 #include<stdio.h>
-
+#include<string.h>
+#include<stdlib.h>
 #pragma warning(disable: 4996)
+
+int stack[10000];
+int size=0;
+
+void push(int data) {
+	if (size > 10000)
+		return;
+
+	stack[size++] = data;
+}
+
+int pop(int data) {
+	if (size == 0)
+		return -1;
+
+	return stack[--size];
+}
+
+int top() {
+	if (size == 0)
+		return -1;
+
+	return stack[size-1];
+}
+
+int empty() {
+	if (size == 0)
+		return 1;
+	else
+		return 0;
+}
+
+int sizeOfStack() {
+	return size;
+}
 
 int main(void)
 {
-	int mon, day, daySum = 0;
+	int num, data;
+	char input[10];
+	scanf("%d", &num);
 
-	char days[7][4] = { "SUN", "MON","TUE","WED","THU", "FRI", "SAT" };
-	char monDate[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	scanf("%d %d", &mon, &day);
+	for (int i = 0; i < num; i++) {
+		scanf("%s", input);
+		if (!strcmp("push", input)) {
+			scanf("%d", &data);
+			push(data);
+		}
+		else if (!strcmp("pop", input)) {
+			printf("%d\n", pop(data));
+		}
+		else if (!strcmp("top", input)) {
+			printf("%d\n", top());
 
-	for (int i = 1; i < mon; i++)
-		daySum += monDate[i - 1];
-
-	daySum += day;
-
-	printf("%s\n", days[daySum % 7]);
+		}
+		else if (!strcmp("empty", input)){
+			printf("%d\n", empty());
+		}
+		else if (!strcmp("size", input)) {
+			printf("%d\n", sizeOfStack());
+		}
+	}
 }

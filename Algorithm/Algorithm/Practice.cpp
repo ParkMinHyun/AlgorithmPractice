fe89cbb8fe7e1638000ef6339fa2fa349e3a68e0
박민현 <algusl92@naver.com>
2017-10-05 13:49:54 +0900

    피보나치 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,20 +1,28 @@
 #include<stdio.h>
+
 #pragma warning(disable: 4996)
 
-int gcd(int m, int n) {
-	if (n == 0)
-		return m;
-
-	return gcd(n, m%n);
+int fibo(int num) {
+	if (num == 1 || num == 0)
+		return num;
+	else
+		return fibo(num - 1) + fibo(num - 2);
 }
 
-int lcm(int m, int n)
-{
-	return (m*n) / gcd(m, n);
+int main(void) {
+
+	int num;
+	int i;
+
+	printf("\n피보나치 수 입력 : ");
+	scanf("%d", &num);
+
+	printf("%d\n", fibo(num));
+
+
+
+	return 0;
+
 }
 
-int main() {
-	int m, n;
-	scanf("%d %d", &m, &n);
-	printf("%d %d\n", gcd(m, n),lcm(m,n));
-}
+

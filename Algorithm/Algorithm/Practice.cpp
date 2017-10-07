b7beaa3e5378d6a36a909c04c274a97f9c013faf
박민현 <algusl92@naver.com>
2017-10-08 00:44:26 +0900

    숫자 삼각형 알고리즘 ( DP 기초 )

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,16 +1,40 @@
 #include<stdio.h>
 #pragma warning(disable: 4996)
 
-int m(int a, int b) { return a<b ? a : b; }
-int N, i, R, G, B, r, g, b;
-int main() {
-	scanf("%d", &N);
-	while (N--) {
-		scanf("%d%d%d", &r, &g, &b);
-		r += m(G, B);
-		g += m(R, B);
-		b += m(R, G);
-		R = r, G = g, B = b;
+int maxSum = 0;
+int tri[500][500];
+int num;
+
+void compare(int floor, int index, int sum) {
+
+	sum += tri[floor][index];
+
+	if (floor == num) {
+		if (sum > maxSum)
+			maxSum = sum;
+		return;
 	}
-	printf("%d", m(m(R, G), B));
+
+
+	for (int i = index; i <= index + 1; i++) {
+		compare(floor + 1, i, sum);
+	}
+}
+
+int main(void) {
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j <= i; j++) {
+			scanf("%d", &tri[i][j]);
+		}
+	}
+
+	int sum = 0;
+	compare(0, 0, sum);
+
+
+	printf("%d\n", maxSum);
+
+	return 0;
 }

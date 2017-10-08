884debd9063a092b5b9e5f1873e5842069c272ed
박민현 <algusl92@naver.com>
2017-10-08 17:15:07 +0900

    유기농 배추 알고리즘

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -2,40 +2,62 @@
 #include<stdlib.h>
 #pragma warning(disable: 4996)
 
-int maxSum = 0;
-int tri[500][500];
-int midSum[500][500] = { 0, };
-int compare(const void *x, const void *y)
-{
-	int *a = (int *)x;
-	int *b = (int *)y;
+int h, w;
+void compare(int bat[][50], int check[][50], int y, int x) {
 
-	return *a > *b ? -1 : 1;
+	// 밭(bat) 배열에 배추있으면 이 지점에서 재귀이용해 4방향 검사
+	// 그리고 check 배열에 배추 있다고 표시
+	if (x - 1 >= 0)
+		if (bat[y][x - 1] == 1 && check[y][x - 1] == 0) {
+			check[y][x - 1] = 1;
+			compare(bat, check, y, x - 1);
+		}
+
+	if (y - 1 >= 0)
+		if (bat[y - 1][x] == 1 && check[y - 1][x] == 0) {
+			check[y - 1][x] = 1;
+			compare(bat, check, y - 1, x);
+		}
+
+	if (x + 1 < w)
+		if (bat[y][x + 1] == 1 && check[y][x + 1] == 0) {
+			check[y][x + 1] = 1;
+			compare(bat, check, y, x + 1);
+		}
+
+	if (y + 1 < h)
+		if (bat[y + 1][x] == 1 && check[y + 1][x] == 0) {
+			check[y + 1][x] = 1;
+			compare(bat, check, y + 1, x);
+		}
 }
 
 int main(void) {
-	int floor, sum = 0;
-	scanf("%d", &floor);
+	int num, x, y, k, sum = 0;
+	scanf("%d", &num);
 
-	for (int i = 0; i < floor; i++) {
-		for (int j = 0; j <= i; j++) {
-			scanf("%d", &tri[i][j]);
+	while (num--) {
+		int bat[50][50] = { 0, };
+		int check[50][50] = { 0, };
+		sum = 0;
+
+		scanf("%d %d %d", &h, &w, &k);
+
+		while (k--) {
+			scanf("%d %d", &y, &x);
+			bat[y][x] = 1;
 		}
-	}
 
-	midSum[0][0] = tri[0][0];
-	for (int i = 0; i < floor - 1; i++) {
-		for (int j = 0; j <= i; j++) {
-			if (midSum[i + 1][j] < midSum[i][j] + tri[i + 1][j])
-				midSum[i + 1][j] = midSum[i][j] + tri[i + 1][j];
-
-			if (midSum[i + 1][j + 1] < midSum[i][j] + tri[i + 1][j + 1])
-				midSum[i + 1][j + 1] = midSum[i][j] + tri[i + 1][j + 1];
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				if (check[i][j] == 0 && bat[i][j] == 1) {
+					compare(bat, check, i, j);
+					sum++;
+				}
+			}
 		}
+		printf("%d\n", sum);
 	}
-
-	qsort(midSum[floor - 1], floor, sizeof(int), compare);
-	printf("%d\n", midSum[floor - 1][0]);
 
 	return 0;
 }

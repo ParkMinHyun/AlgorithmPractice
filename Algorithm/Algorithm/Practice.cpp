3957c26ad63363f26684d6c55f8718ac89d1c1dd
박민현 <algusl92@naver.com>
2017-10-09 12:42:11 +0900

    농구 경기 알고리즘 ( 1159번 )

--- Algorithm/Algorithm/Practice.cpp
+++ Algorithm/Algorithm/Practice.cpp
@@ -1,63 +1,27 @@
 #include<stdio.h>
-#include<stdlib.h>
 #pragma warning(disable: 4996)
 
-int h, w;
-void compare(int bat[][50], int check[][50], int y, int x) {
-
-	// 밭(bat) 배열에 배추있으면 이 지점에서 재귀이용해 4방향 검사
-	// 그리고 check 배열에 배추 있다고 표시
-	if (x - 1 >= 0)
-		if (bat[y][x - 1] == 1 && check[y][x - 1] == 0) {
-			check[y][x - 1] = 1;
-			compare(bat, check, y, x - 1);
-		}
-
-	if (y - 1 >= 0)
-		if (bat[y - 1][x] == 1 && check[y - 1][x] == 0) {
-			check[y - 1][x] = 1;
-			compare(bat, check, y - 1, x);
-		}
-
-	if (x + 1 < w)
-		if (bat[y][x + 1] == 1 && check[y][x + 1] == 0) {
-			check[y][x + 1] = 1;
-			compare(bat, check, y, x + 1);
-		}
-
-	if (y + 1 < h)
-		if (bat[y + 1][x] == 1 && check[y + 1][x] == 0) {
-			check[y + 1][x] = 1;
-			compare(bat, check, y + 1, x);
-		}
-}
-
 int main(void) {
-	int num, x, y, k, sum = 0;
+	int num,flag=0;
+	int alphabet[26] = { 0, };
+
 	scanf("%d", &num);
-
 	while (num--) {
-		int bat[50][50] = { 0, };
-		int check[50][50] = { 0, };
-		sum = 0;
+		char name[31];
+		scanf("%s", name);
 
-		scanf("%d %d %d", &h, &w, &k);
-
-		while (k--) {
-			scanf("%d %d", &y, &x);
-			bat[y][x] = 1;
-		}
-
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if (check[i][j] == 0 && bat[i][j] == 1) {
-					compare(bat, check, i, j);
-					sum++;
-				}
-			}
-		}
-		printf("%d\n", sum);
+		alphabet[name[0] - 97] ++;
 	}
+
+	for (int i = 0; i < 26; i++) {
+		if (alphabet[i] >= 5) {
+			printf("%c", i + 97);
+			flag = 1;
+		}
+	}
+
+	if (flag == 0)
+		printf("PREDAJA");
 
 	return 0;
 }

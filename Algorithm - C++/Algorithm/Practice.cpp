6b333de5382433c231a430dba73ea65101719764
박민현 <algusl92@naver.com>
2017-10-30 15:49:54 +0900

    중복 숫자 제거 알고리즘

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <vector>
+#include <list>
 #include <algorithm>
 #include <iostream>
 using namespace std;
@@ -8,27 +9,20 @@ using namespace std;
 
 int main() {
 
-	typedef pair <int, int> ii;
-	vector <ii> v;
-	int n, k;
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
+	list <int> v;
+	vector<int> temp;
+	int num, k;
+	cin >> num;
+	for (int i = 1; i <= num; i++) {
 		cin >> k;
-		v.push_back(ii(k, i));
+		auto it = find(v.begin(), v.end(), k);
+		if( it == v.end())
+			v.push_back(k);
 	}
 
-	int s = 0, l = n, p;
-	for (int i = 0; i<n; i++) {
-		printf("%d ", v[s].second);
-		p = v[s].first;
-		p += (p>0) ? -1 : 0;
-		v.erase(v.begin() + s);
-		s += p; 
-		l--;
-		if (!l) 
-			break;
-		if (s<0)
-			s = l + s%l;
-		s %= l;
+	v.sort();
+	for (int x : v) {
+		cout << x << ' ';
 	}
+
 }

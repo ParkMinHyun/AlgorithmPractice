bfa5e4441ef70acaf247cbf1efec8724c4809a36
박민현 <algusl92@naver.com>
2017-11-01 01:32:16 +0900

    저항 알고리즘 - map 이용 ( 1076번 )

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,45 +1,78 @@
-#include <string>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <utility>
+#include <algorithm>
 using namespace std;
 #pragma warning(disable : 4996)
 
-int main() {
-	string text;
-	int num;
-
-	cin >> text >> num;
-
-	int cursorIndex = text.size();
-	while (num--) {
-		char input;
-		string t;
-
-		cin >> input;
-		switch (input)
-		{
-		case 'P':
-			cin >> t;
-			text.insert(cursorIndex, t);
-			cursorIndex++;
-			break;
-		case 'L':
-			if (cursorIndex != 0)
-				cursorIndex--;
-			break;
-		case 'D':
-			if (cursorIndex != text.size())
-				cursorIndex++;
-			break;
-		case 'B':
-			if (cursorIndex != 0) {
-				text.erase(cursorIndex - 1, 1);
-				cursorIndex--;
-			}
-			break;
-		default:
-			break;
-		}
-		cout << cursorIndex << ' ' << text << '\n';
+int findColor(string str) {
+	if (str._Equal("black")) {
+		return 0;
 	}
-	cout << text << '\n';
+	else if (str._Equal("brown")) {
+		return 1;
+	}
+	else if (str._Equal("red")) {
+		return 2;
+
+	}
+	else if (str._Equal("orange")) {
+		return 3;
+
+	}
+	else if (str._Equal("yellow")) {
+		return 4;
+
+	}
+	else if (str._Equal("green")) {
+		return 5;
+
+	}
+	else if (str._Equal("blue")) {
+		return 6;
+
+	}
+	else if (str._Equal("violet")) {
+		return 7;
+
+	}
+	else if (str._Equal("grey")) {
+		return 8;
+
+	}
+	else if (str._Equal("white")) {
+		return 9;
+
+	}
+}
+int main() {
+	typedef pair<int, int> pa;
+	vector<pa> vec;
+
+	int ten = 1;
+	for (int i = 1; i <= 10; i++) {
+		vec.push_back(make_pair(i - 1, ten));
+		ten *= 10;
+	}
+
+	long long sum;
+	for(int i=0; i<3; i++){
+		string a;
+		cin >> a;
+
+		int temp = findColor(a);
+		if (i == 0) {
+			sum = vec[temp].first;
+		}
+		else if (i == 1) {
+			sum *= 10;
+			sum += vec[temp].first;
+		}
+		else {
+			sum *= vec[temp].second;
+		}
+	}
+	printf("%lld", sum);
 }

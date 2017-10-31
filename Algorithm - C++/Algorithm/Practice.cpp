f8d7a599f3867ebf2724636b687ba4df17b4b104
박민현 <algusl92@naver.com>
2017-11-01 00:23:33 +0900

    에디터 알고리즘 ( 1406번 )

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,64 +1,45 @@
-#include <cstdio>
-#include <vector>
-#include <list>
-#include <set>
-#include <utility>
-#include <algorithm>
+#include <string>
 #include <iostream>
 using namespace std;
 #pragma warning(disable : 4996)
 
-typedef pair<int, int> pa;
-vector<pa> vec;
-void bs(int k) {
-	int left = 0;
-	int right = vec.size() - 1;
-	int mid;
-
-	while (left <= right) {
-		mid = (left + right) / 2;
-		if (vec[mid].first < k) {
-			left = mid + 1;
-		}
-		else if (vec[mid].first > k)
-			right = mid - 1;
-		else
-		{
-			printf("%d ",vec[mid].second);
-			return;
-		}
-	}
-
-	printf("0 ");
-
-}
-
 int main() {
+	string text;
+	int num;
 
+	cin >> text >> num;
 
-	int num1, num2, k;
-	scanf("%d", &num1);
-	while (num1--)
-	{
-		int i = 0;
-		cin >> k;
+	int cursorIndex = text.size();
+	while (num--) {
+		char input;
+		string t;
 
-		for (i = 0; i < vec.size(); i++) {
-			if (vec[i].first == k){
-				vec[i].second++;
-				break;
+		cin >> input;
+		switch (input)
+		{
+		case 'P':
+			cin >> t;
+			text.insert(cursorIndex, t);
+			cursorIndex++;
+			break;
+		case 'L':
+			if (cursorIndex != 0)
+				cursorIndex--;
+			break;
+		case 'D':
+			if (cursorIndex != text.size())
+				cursorIndex++;
+			break;
+		case 'B':
+			if (cursorIndex != 0) {
+				text.erase(cursorIndex - 1, 1);
+				cursorIndex--;
 			}
+			break;
+		default:
+			break;
 		}
-		if (vec.size() == 0 || i == vec.size())
-			vec.push_back(make_pair(k, 1));
+		cout << cursorIndex << ' ' << text << '\n';
 	}
-
-	sort(vec.begin(), vec.end());
-	scanf("%d", &num2);
-	for (int i = 0; i < num2; i++)
-	{
-		cin >> k;
-		bs(k);
-	}
-
+	cout << text << '\n';
 }

bbe2570b72661cbf9f5e6d5abcab256a0260b822
박민현 <algusl92@naver.com>
2017-11-01 02:56:55 +0900

    듣보잡 알고리즘 ( 1764번 )

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,78 +1,36 @@
-#include <iostream>
-#include <vector>
 #include <string>
-#include <cmath>
-#include <utility>
+#include <cstdio>
+#include <vector>
+#include <iostream>
 #include <algorithm>
+
 using namespace std;
-#pragma warning(disable : 4996)
 
-int findColor(string str) {
-	if (str._Equal("black")) {
-		return 0;
+int N, M;
+vector<string> listen;
+vector<string> print;
+
+int main()
+{
+	int cnt = 0;
+	char arr[21];
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i) {
+		cin >> arr;
+		listen.push_back(arr);
 	}
-	else if (str._Equal("brown")) {
-		return 1;
-	}
-	else if (str._Equal("red")) {
-		return 2;
+	sort(listen.begin(), listen.end());
 
-	}
-	else if (str._Equal("orange")) {
-		return 3;
-
-	}
-	else if (str._Equal("yellow")) {
-		return 4;
-
-	}
-	else if (str._Equal("green")) {
-		return 5;
-
-	}
-	else if (str._Equal("blue")) {
-		return 6;
-
-	}
-	else if (str._Equal("violet")) {
-		return 7;
-
-	}
-	else if (str._Equal("grey")) {
-		return 8;
-
-	}
-	else if (str._Equal("white")) {
-		return 9;
-
-	}
-}
-int main() {
-	typedef pair<int, int> pa;
-	vector<pa> vec;
-
-	int ten = 1;
-	for (int i = 1; i <= 10; i++) {
-		vec.push_back(make_pair(i - 1, ten));
-		ten *= 10;
-	}
-
-	long long sum;
-	for(int i=0; i<3; i++){
-		string a;
-		cin >> a;
-
-		int temp = findColor(a);
-		if (i == 0) {
-			sum = vec[temp].first;
-		}
-		else if (i == 1) {
-			sum *= 10;
-			sum += vec[temp].first;
-		}
-		else {
-			sum *= vec[temp].second;
+	for (int i = 0; i < M; ++i) {
+		cin >> arr;
+		if (binary_search(listen.begin(), listen.end(), arr)) {
+			print.push_back(arr);
+			cnt++;
 		}
 	}
-	printf("%lld", sum);
+
+	sort(print.begin(), print.end());
+	printf("%d\n", cnt);
+	for (int i = 0; i < print.size(); i++) 
+		printf("%s\n", print[i].c_str());
 }

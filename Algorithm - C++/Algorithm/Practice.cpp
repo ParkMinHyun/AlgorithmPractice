ba376db0c4fc99308e6d68576122df826c4b26ef
박민현 <algusl92@naver.com>
2017-11-04 00:56:03 +0900

    공 넣기 알고리즘 - map 이용 ( 10810번 )

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
+#include <malloc.h>
+#include <algorithm>
 using namespace std;
 
 int main(void) {
-	string s;
-	int sum = 0;
-	while (getline(cin, s, ',')) {
-		sum += stoi(s);
+	int M, N, a,b,c;
+	map<int, int> m;
+	cin >> N >> M;
+
+	while (M--) {
+		cin >> a >> b >> c;
+		for (int i = a - 1; i < b; i++)
+			m[i] = c;
 	}
-	cout << sum << endl;
+
+	for (int i = 0; i < N; i++)
+		cout << m[i] << ' ';
+
+
 }

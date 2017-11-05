d9c9c926fefbbc20c64eb68cb92a12d4f82ada71
박민현 <algusl92@naver.com>
2017-11-06 03:15:18 +0900

    캐시 알고리즘 ( LRU 교체 )

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,53 +1,34 @@
 #include <iostream>
+#include <map>
+#include <list>
+#include <vector>
 #include <string>
 #include <math.h>
 using namespace std;
 
 int main(void) {
-	int score[3] = { 0 };
-	int sum = 0, index = 0;
-	string str;
+	int cacheSize,time=0;
+	string cityName;
 
-	cin >> str;
-	for (int i = 0; i < str.length(); i++) {
-		int temp = 0;
-		if (str[i] >= '0' && str[i] <= '9')
-		{
-			// 10일 경우 temp에 10 저장후 i+1 만큼 이동
-			if (str[i] == '1' && str[i + 1] == '0'){
-				temp = 10;
-				i++;
-			}
-			// 1~9일 경우 temp에 해당 점수 저장
-			else
-				temp = str[i] - 48;
-
-			// 그 뒤 S,D,T 판별 후 Score 변수에 저장
-			switch (str[i + 1])
-			{
-			case 'S':score[index++] = temp; break;
-			case 'D':score[index++] = pow(temp, 2); break;
-			case 'T':score[index++] = pow(temp, 3); break;
-			default: break;
-			}
-
-			// 옵션 계산
-			switch (str[i + 2])
-			{
-			case '*':
-				score[index - 1] *= 2;
-				if (index >= 2)
-					score[index - 2] *= 2;
-				break;
-			case '#':
-				score[index - 1] *= -1; 
-				break;
-			default: break;
-			}
+	scanf("%d ", &cacheSize);
+	list<string> cities;
+	
+	while (getline(cin, cityName, ',')) {
+		auto it = find(cities.begin(), cities.end(), cityName);
+		if (it == cities.end()){
+			if (cities.size() >= cacheSize)
+				cities.erase(--it);
+			time += 5;
 		}
+		else {
+			cities.erase(it);
+			time += 1;
+		}
+		cities.insert(cities.begin(), cityName);
+		cout << time << endl;
 	}
-	for (int i = 0; i < 3; i++)
-		sum += score[i];
-
-	cout << sum << '\n';
+	/*3
+	Jeju,Pangyo,Seoul,NewYork,LA,Jeju,Pangyo,Seoul,NewYork,LA*/
+	/*3
+	Jeju,Pangyo,Seoul,Jeju,Pangyo,Seoul,Jeju,Pangyo,Seoul*/
 }

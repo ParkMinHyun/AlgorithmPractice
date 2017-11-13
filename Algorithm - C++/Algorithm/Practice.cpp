02a84a252b3d9c926ac00992bc6ca46439240753
박민현 <algusl92@naver.com>
2017-11-14 01:23:17 +0900

    비밀지도 알고리즘 - bitset 이용

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,17 +1,30 @@
-#include <algorithm>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <bitset>
 using namespace std;
 int main() {
+	int n, input;
+	cin >> n;
 
-	vector<int> a = { 1, 3, 4, 5, 7, 7, 8 };
-	int max = *max_element(a.begin(), a.end());
-	for(int i=1; i<=max; i++){
-		auto one = equal_range(a.begin(), a.end(), i).first;
-		auto two = equal_range(a.begin(), a.end(), i).second;
-		cout << two - one << '\n';
+	// bitset 배열 생성 및 입력
+	vector<bitset<16>> arr1, arr2;
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		arr1.push_back(input);
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		arr2.push_back(input);
+	}
 
-		cout << count(a.begin(),a.end(),i) << '\n';
-		cout << "==========================\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = n - 1; j >= 0; j--) {
+			// or 비트 연산자 
+			if (arr1[i].test(j) | arr2[i].test(j))
+				cout << "#";
+			else
+				cout << " ";
+		}
+		cout << '\n';
 	}
 }

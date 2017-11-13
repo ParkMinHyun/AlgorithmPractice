2844fae146bd2f2b748a1d1b87b40b7610a4d585
박민현 <algusl92@naver.com>
2017-11-13 19:33:59 +0900

    차이를 최대로 알고리즘 - next_permutation 이용 ( 10819번 )

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,10 +1,31 @@
 #include <algorithm>
+#include <vector>
 #include <iostream>
 using namespace std;
 int main() {
-	int a, b, c;
-	cin >> a >> b >> c;
-	auto p = minmax({ a, b, c });
-	cout << (a + b + c) - p.first - p.second << '\n';
-	return 0;
+	int N;
+	cin >> N;
+
+	vector<int> a;
+	for (int i = 0; i < N; i++) {
+		int input;
+		cin >> input;
+		a.push_back(input);
+	}
+
+	int maxSum = 0;
+	sort(a.begin(), a.end());
+	do {
+		int sum = 0;
+		for (int i = 0; i < N - 1; i++)
+		{
+			sum += abs(a[i] - a[i + 1]);
+		}
+		if (maxSum < sum)
+			maxSum = sum;
+
+	} while (next_permutation(a.begin(), a.end()));
+
+
+	cout << maxSum << '\n';
 }

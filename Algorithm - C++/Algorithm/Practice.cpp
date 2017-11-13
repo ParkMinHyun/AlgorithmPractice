75ec986d2936a84c82b6117c1149120bcab842d4
박민현 <algusl92@naver.com>
2017-11-13 17:42:41 +0900

    나이순 정렬 알고리즘 - stable_sort() 이용 ( 10814번 )

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -7,11 +7,12 @@ using namespace std;
 
 struct Person {
 	string name;
-	int age,order;
+	int age;
 };
 bool cmp(const Person &u, const Person &v) {
-	return make_pair(u.age, u.order) < make_pair(v.age, v.order);
+	return (u.age) < (v.age);
 }
+
 int main(void) {
 	int n;
 	cin >> n;
@@ -19,10 +20,9 @@ int main(void) {
 	vector<Person> a(n);
 	for (int i = 0; i < n; i++){
 		cin >> a[i].age >> a[i].name;
-		a[i].order = i;
 	}
 
-	sort(a.begin(), a.end(), cmp);
+	stable_sort(a.begin(), a.end(), cmp);
 
 	for (Person x : a) {
 		cout <<  x.age << ' ' <<  x.name << '\n';

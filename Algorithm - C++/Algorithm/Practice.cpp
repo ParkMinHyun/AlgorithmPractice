7bd90dbcbaca5827b853e5f3bad6886ed591e5f1
박민현 <algusl92@naver.com>
2017-11-13 14:57:55 +0900

    바구니 뒤집기 알고리즘 - reverse() 이용 ( 10811번 )

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -5,32 +5,20 @@
 using namespace std;
 int main(void) {
 	int N, M;
-	int a, b, c;
+	int a, b;
 	cin >> N >> M;
-	vector<int> d(N);
-	fill(d.begin(), d.end(), 0);
-	
-	while (M--) {
-		cin >> a >> b >> c;
-		fill(d.begin() + (a - 1), d.begin() + b, c);
-
-		for (int x : d) {
-			cout << x << ' ';
-		}
-		cout << '\n';
+	vector<int> d;
+	for(int i=1; i<=N;i++) {
+		d.push_back(i);
 	}
 
+	while (M--) {
+		cin >> a >> b;
+		reverse(d.begin() + (a - 1), d.begin() + (b));
+	}
 
-	/*int N, M;
-	cin >> N >> M;
-	vector<int> a(N);
-	for (int x : a) {
+	for (int x : d) {
 		cout << x << ' ';
 	}
 	cout << '\n';
-	fill(a.begin(), a.end(), 0);
-	for (int x : a) {
-		cout << x << ' ';
-	}
-	cout << '\n';*/
 }

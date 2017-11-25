350b4ec09edba7f51c38e76e0902c50da3f0f9b1
박민현 <algusl92@naver.com>
2017-11-25 16:21:10 +0900

    Queue 구현 알고리즘 - Stack 2개 이용

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,59 +1,43 @@
 #include <iostream>
-#include <vector>
 #include <string>
-
-#define MAXNUM 255
+#include <stack>
+#include <vector>
 using namespace std;
 
-bool selectWeight(const int data[][2], int result[], int N, int W) {
+class Queue {
+public:
+	stack<int> st1;
+	stack<int> st2;
 
-	int num=0;
-	bool flag = false;
-	for (int i = N-1; i >= 0 ; i--) {
-		for (int j = 0; j < data[i][1]; j++) {
-			if (W >= data[i][0] && W - data[i][0] >= 0) {
-				W -= data[i][0];
-				result[i] ++;
-				flag = true;
+	void enqueue(int v) {
+		st1.push(v);
+	}
+
+	int dequeue() {
+		if (st2.empty()) {
+			while (!st1.empty()) {
+				st2.push(st1.top());
+				st1.pop();
 			}
 		}
+		return st2.top();
 	}
-
-	if (flag == true)
-	{
-		if (W != 0)
-			return false;
-
-		return true;
-	}
-	return false;
-}
+};
 
 int main(int argc, const char *argv[]) {
-	int TC, W, N;
-	cin >> TC;
-
-	for (int tc = 1; tc <= TC; ++tc) {
-		cin >> W;
-		cin >> N;
-
-		int data[MAXNUM][2];
-		int result[MAXNUM];
-
-		for (int i = 0; i < N; ++i) {
-			cin >> data[i][0] >> data[i][1];
-			result[i] = 0;
+	Queue queue;
+	string command;
+	int count, value;
+	cin >> count;
+	for (int i = 0; i < count; ++i) {
+		cin >> command;
+		if (command == "ENQUEUE") {
+			cin >> value;
+			queue.enqueue(value);
 		}
-
-		cout << "#" << tc << endl;
-		if (!selectWeight(data, result, N, W)) {
-			cout << "FAIL" << endl;
-		}
-		else {
-			for (int i = 0; i < N; ++i) {
-				cout << data[i][0] << " " << result[i] << endl;
-			}
+		else if (command == "DEQUEUE") {
+			cout << queue.dequeue() << endl;
+			queue.st2.pop();
 		}
 	}
-	return 0;
 }

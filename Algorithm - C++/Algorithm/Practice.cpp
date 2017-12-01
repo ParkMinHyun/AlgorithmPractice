f625b557a16c5eac2ab71a6d918b4296b970aab1
박민현 <algusl92@naver.com>
2017-12-01 14:59:48 +0900

    선택 정렬 알고리즘

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -3,18 +3,42 @@
 #include <iostream>
 
 using namespace std;
+void quickSort(int arr[], int originLeft, int originRight) {
+	
+	int pivot;
+	for (int i = 0; i < 10; i++) {
+		pivot = i;
+		for (int j = i+1; j < 10; j++) {
+			if (arr[pivot] < arr[j])
+			{
+				pivot = j;
+			}
+		}
+
+		int temp = arr[pivot];
+		arr[pivot] = arr[i];
+		arr[i] = temp;
+	}
+
+
+}
+
 int main() {
 
-	int a;
-	char b[100];
+	int arr[10] = { 10,1,1,16,9,55,2,5,22,7 };
 
-	cin >> a;
 
-	int index = 0;
-	while (a) {
-		b[index++] = a % 10 + '0';
-
-		a /= 10;
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", arr[i]);
 	}
-	printf("%s", b);
+
+	cout << endl;
+
+	quickSort(arr, 0, 9);
+
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", arr[i]);
+	}
+
+
 }

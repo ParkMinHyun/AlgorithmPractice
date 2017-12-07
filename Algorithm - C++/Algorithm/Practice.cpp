cd3014a9c9f441e78673a41eca8d3144e4e5a6d2
박민현 <algusl92@naver.com>
2017-12-07 15:59:01 +0900

    중복 제거 알고리즘 - 정렬 X

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,60 +1,25 @@
-#include <string>
-#include <iostream>
-#include <stack>
-using namespace std;
-
-void quickSort(int num[], int originLeft, int originRight)
-{
-	int left = originLeft;
-	int right = originRight;
-	int pivot = num[(left + right) / 2];
-	int temp;
-
-	while (left <= right) {
-		while (num[left] < pivot)
-			left++;
-
-		while (num[right] > pivot)
-			right--;
-
-		if (left <= right) {
-			temp = num[left];
-			num[left] = num[right];
-			num[right] = temp;
-
-			left++;
-			right--;
-		}
-	}
-
-	if (left < originRight)
-		quickSort(num, left, originRight);
-
-	if (right > originLeft)
-		quickSort(num, originLeft, right);
-
-}
-
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdio.h>
 
 void printWithoutOverlapNum(int num[], int size)
 {
+	int checkNum[1000] = { 0 };
+
 	for (int i = 0; i < size; i++)
 	{
-		if (num[i] != num[i + 1])
-			cout << num[i] << ' ';
+		checkNum[num[i]] ++;
+		if (checkNum[num[i]] <= 1)
+			printf("%d ", num[i]);
 	}
-	cout << endl;
+	printf("\n");
 }
 
 int main() {
 
 	int arr[10] = { 11, 1, 3,40,123,40,12,3,55,1 };
 
-	for (int num : arr)
-		cout << num << ' ';
-
-	quickSort(arr, 0, 9);
-	cout << endl;
 	printWithoutOverlapNum(arr, 10);
 
 }

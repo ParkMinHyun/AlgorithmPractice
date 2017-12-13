33843aa25cd42201fc6ca3baf7ef4a088a2b791f
박민현 <algusl92@naver.com>
2017-12-13 23:42:23 +0900

    길 찾기 경로의 수 알고리즘

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,20 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-	int num = 0;
-	int binary[30];
+	int **path;
 
-	int index = 0;
-	scanf("%d", &num);
+	int w, h;
+	scanf("%d %d", &w, &h);
 
-	while (num) {
-		binary[index ++] = num % 2;
-		num /= 2;
-	}
+	path = (int **)malloc(sizeof(int)*h);
+	for (int i = 0; i < h; i++)
+		path[i] = (int *)malloc(sizeof(int)*w);
 
-	for (int i = index - 1; i >= 0; i--)
-		printf("%d",binary[i]);
+	for (int i = 0; i < h; i++)
+		path[i][0] = 1;
 
-	printf("\n");
+	for (int i = 0; i < w; i++)
+		path[0][i] = 1;
+
+	for (int i = 1; i < h; i++)
+		for (int j = 1; j < w; j++)
+			path[i][j] = path[i][j - 1] + path[i - 1][j];
+
+	printf("%dX%d 크기의 가능한 경로 수는: %d\n", w,h, path[h-1][w-1]);
+
+
+	
 }

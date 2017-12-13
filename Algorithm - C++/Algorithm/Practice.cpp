3d068c55d45192e6f9ee1a5ff757ee2b20ab881e
박민현 <algusl92@naver.com>
2017-12-14 00:48:32 +0900

    길 찾기 경로의 수 알고리즘 - 재귀

--- Algorithm - C++/Algorithm/Practice.cpp
+++ Algorithm - C++/Algorithm/Practice.cpp
@@ -1,29 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int width, height;
+void find(int **path, int h, int w) {
+
+	if (h == height)
+		return;
+
+	if (w == width)
+		return;
+
+	path[h][w] = path[h - 1][w] + path[h][w - 1];
+
+	find(path, h + 1, w);
+	find(path, h, w + 1);
+}
+
 int main() {
 
 	int **path;
 
-	int w, h;
-	scanf("%d %d", &w, &h);
+	scanf("%d %d", &height, &width);
 
-	path = (int **)malloc(sizeof(int)*h);
-	for (int i = 0; i < h; i++)
-		path[i] = (int *)malloc(sizeof(int)*w);
+	path = (int **)malloc(sizeof(int)*height);
+	for (int i = 0; i < height; i++)
+		path[i] = (int *)malloc(sizeof(int)*width);
 
-	for (int i = 0; i < h; i++)
+	for (int i = 0; i < height; i++)
 		path[i][0] = 1;
 
-	for (int i = 0; i < w; i++)
-		path[0][i] = 1;
+	for (int j = 0; j < width; j++)
+		path[0][j] = 1;
 
-	for (int i = 1; i < h; i++)
-		for (int j = 1; j < w; j++)
-			path[i][j] = path[i][j - 1] + path[i - 1][j];
-
-	printf("%dX%d 크기의 가능한 경로 수는: %d\n", w,h, path[h-1][w-1]);
-
-
-	
+	find(path, 1, 1);
+	printf("%dX%d 크기의 가능한 경로 수는: %d\n", width, height, path[height-1][width-1]);
 }
